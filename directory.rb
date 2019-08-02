7c2391aa09cdcc80c2b3279131486adaf20db1c9
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-08-02 11:27:40 +0100

    put students in array etc.

---
+++ directory.rb
@@ -0,0 +1,21 @@
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates",
+]
+
+puts "The students of Villains Academy"
+puts "-------------"
+students.each { |student|
+  puts student
+}
+
+puts "Overall, we have #{students.count} great students"
