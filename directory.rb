cc2d7ee1a03c53d0c6b31288072d2cee9b61ebcb
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-08-02 11:45:41 +0100

    updated print method to reflect changed students structure (from array to hash)

--- directory.rb
+++ directory.rb
@@ -12,6 +12,19 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
